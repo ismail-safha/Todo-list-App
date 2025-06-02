1b5bbd085d47bb4c8ae15ef85b6a66a8a76b57ef
ismail <ismailsafha@gmail.com>
2025-06-02 17:21:46 +0200

    fix build failed in vercel deployment

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,39 +1,7 @@
 import { prisma } from "@/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-// //Updates the 'done' status of a task
-// export async function PATCH(
-//   req: NextRequest,
-//   context: { params: Promise<{ id: string }> }
-// ) {
-//   const params = await context.params; // <-- await here!
-//   const { id } = params;
-
-//   const body = await req.json();
-//   const { done } = body;
-
-//   const updatedTask = await prisma.task.update({
-//     where: { id: Number(id) },
-//     data: { done },
-//   });
-
-//   return NextResponse.json(updatedTask);
-// }
-
-// DELETE: delete task by id
-// export async function DELETE(
-//   req: NextRequest,
-//   context: { params: Promise<{ id: string }> }
-// ) {
-//   const params = await context.params;
-//   const { id } = params;
-
-//   const deletedTask = await prisma.task.delete({
-//     where: { id: Number(id) },
-//   });
-
-//   return NextResponse.json(deletedTask);
-// }
+//Updates the 'done' status of a task
 
 export async function PATCH(
   req: NextRequest,
@@ -78,7 +46,3 @@ export async function DELETE(
 
   return Response.json("Deleted Successfully", { status: 200 });
 }
-
-//===
-
-// // PATCH
